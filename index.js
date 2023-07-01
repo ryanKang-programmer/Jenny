2e53da417e61b82b4da910c52814c138508c80a0
ryankang-programmer <=>
2023-06-30 18:59:30 -0600

    Add images

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ window.onload = () => {
      * Making navigation menus
      */
 
-    if (window.outerWidth < 900) {
-        document.querySelector("html").style.height = window.innerHeight + "px";
-        document.querySelector("body").style.height = window.innerHeight + "px";
-    }
-
     const nav = document.getElementById('navigation');
 
     fetch('./data/navigation_menu.json')
